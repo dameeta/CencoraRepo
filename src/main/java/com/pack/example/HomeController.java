786e47a1cc54ac0b54722b237a7a05a598438270
Manisha Shah <aksharinfo009@gmail.com>
2024-08-26 14:20:18 +0530

    Java Config Example with spring mvc

---
+++ src/main/java/com/pack/example/HomeController.java
@@ -0,0 +1,16 @@
+package com.pack.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/home",REQUEST.method = "GET")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to Spring MVC!");
+        return "home";  // This refers to /WEB-INF/views/home.jsp
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,9 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>${message}</h1>
+</body>
+</html>
